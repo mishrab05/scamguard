3ef14d4ffbb5d0f3f925435d015452312e99de4c
mishrab05 <mrab0086@student.monash.edu>
2024-04-07 16:56:04 +1000

    modified detect scam page css

--- src/routes/Detectscam.js
+++ src/routes/Detectscam.js
@@ -16,41 +16,52 @@ const Detectscam =()=>{
 
   return(
     <div>
-      <section style={{
+        <section style={{
         backgroundImage: `url(${Image})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundAttachment: 'fixed',
-        backgroundPosition: 'center center'
-      }}> 
-      <Navbar/>
+        backgroundBlendMode: 'darken',
+        }}>
+        <Navbar/>
+
+
+        <section class="search-tool">
+            <div class="box-content">
+                <div>
+                    <h1 class="centered-text">
+                        A free tool to identify a scam text message
+                    </h1>
+                    <p class="centered-text">
+                        Simply copy a text message into the box below and click SEARCH
+                    </p>
+                    <div className="search-box">
+                        <input type="text" id="messageInput" placeholder="Enter text message here." value={message} onChange={(e) => setMessage(e.target.value)} />
+                    </div>
+                    <div>
+                        <button className="searchButton"onClick={handleSearch}>Search</button>
+                    </div>
+                </div>
+            </div>
+		    </section>
       
-      
-        <section className="search-tool">
-          <div className="tracker-message">
-            <p className="tracker-text">A free tool to identify scam text message.</p>
-            <p className="tracker-text">Simply copy a text message into the box below and click SEARCH.</p>
-            <input type="text" id="messageInput" placeholder="Enter text message" value={message}
-              onChange={(e) => setMessage(e.target.value)} />
-            <button className="searchButton"onClick={handleSearch}>Search</button>
-          </div>
+        <section className="result-section">
+            <div id="searchResults" className="search-results">
+                <p>Search Results</p>
+            </div>
         </section>
 
         <section className="result-section">
-          <div id="searchResults" className="search-results">
-            <p>Search results will appear here</p>
-          </div>
-
-          <div className="result-actions">
-            <button className="report-scam">Report This</button>
-            <button className="learn-more" onClick={() => navigate('/help')}>Learn More</button>
-            <button className="homeButton" onClick={() => navigate('/')}>Home Page</button>
-            <button className="nextStepsButton">Next Steps</button>
-          </div>
+            <div className="result-actions">
+            <button className="homeButton" onClick={() => navigate('/')}>Home</button>
+              <button className="report-scam" onClick={() => navigate('/helpsupport')}>Report This</button>
+              <button className="learn-more" onClick={() => navigate('/helpsupport')}>Learn More</button>
+              <button className="nextStepsButton">Next Steps</button>
+            </div>
         </section>
     
-      </section>
-      </div>
+        </section>
+    </div>
   );
 
 }
