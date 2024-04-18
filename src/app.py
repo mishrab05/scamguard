b75fab661b645953457e087c9655c71bcc9394f8
iris-Tian <stia0018@student.monash.edu>
2024-04-18 18:13:50 +1000

    Add some proxy setting.

--- src/app.py
+++ src/app.py
@@ -184,5 +184,5 @@ def latest_report():
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='0.0.0.0',debug=True)
 
