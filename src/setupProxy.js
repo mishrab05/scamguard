838a6c2c94631fecfc3d3d55cf286f729ca3a8f9
iris-Tian <stia0018@student.monash.edu>
2024-04-23 00:58:14 +1000

    Change IP address.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
     app.use('/api', proxy.createProxyMiddleware({
-        target: 'http://172.214.52.33',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         pathRewrite: {
             '^/api': ''
